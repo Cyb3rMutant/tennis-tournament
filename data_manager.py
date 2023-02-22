1622c0656c90da358f6b741da38fbe2c59e5cda7
Amro <ak2-hassanmahm>
2023-02-22 21:57:28 +0000

    made base for data extractor classes and method for tournament data extraction

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,10 @@
+from abc import *
 from os import listdir
 from os.path import isdir, isfile
 import pathlib
+from docx import Document
+
+DATA_PATH = f"{pathlib.Path(__file__).parent.resolve()}/data"
 
 class ModeNotFoundError(Exception):
     pass
@@ -60,9 +64,56 @@ class DataManager:
         files = [f for f in listdir(self._data_path) if (f.endswith(f".{self._mode}") and isfile(self._data_path+f))]
         return files
 
+class DataExtractor(metaclass=ABCMeta):
+
+    def __init__(self) -> None:
+        self.dm = DataManager()
+        self.dm.set_data_path(DATA_PATH)
+
+class DataExtractorCSV(DataExtractor):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.dm.set_mode("csv")
+    
+
+
+class DataExtractorDOCX(DataExtractor):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.dm.set_mode("docx")
+    
+    def get_tournament_difficulty(self):
+        '''
+        Returns a dictionary containing the tournament name and the tournament difficulty as key value pairs respectively.
+        '''
+        tournament_dict = {}
+        for doc in self.dm.get_data_files():
+            if(doc == "DEGREE OF DIFFICULTY PER TOURNAMENT.docx"):
+                document = Document(f"{DATA_PATH}/{doc}")
+                # go line by line through the document
+                for i in range(len(document.paragraphs)):
+                    # for every line split the text to get the tournament name and difficulty
+                    tournament = document.paragraphs[i].text
+                    tournament = tournament.split(" – degree of difficulty ")
+                    # add the tournament name and difficulty to the tournament dictionary if they exist on that line
+                    if(len(tournament) > 1):
+                        tournament_dict[tournament[0]] = tournament[1]
+        return tournament_dict
+        
+
 if(__name__ == "__main__"):
-    dm = DataManager()
-    dm.set_mode("csv")
-    print(dm.get_mode())
-    dm.set_data_path(f"{pathlib.Path(__file__).parent.resolve()}/data")
-    print(dm.get_data_files())
+    # dm = DataManager()
+    # dm.set_mode("docx")
+    # print(dm.get_mode())
+    # dm.set_data_path(DATA_PATH)
+    # print(dm.get_data_files())
+
+    # document = Document(f"{DATA_PATH}/DEGREE OF DIFFICULTY PER TOURNAMENT.docx")
+    # for p in document.paragraphs:
+    #     print(p.text)
+
+    de = DataExtractorDOCX()
+    print(de.get_tournament_difficulty())
+    
