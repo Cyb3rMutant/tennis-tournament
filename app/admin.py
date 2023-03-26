db0a70991923ad6d17b74b145dafc4297a9a1c05
Yazeed <yazeed2.abu-hummos@live.uwe.ac.uk>
2023-03-26 14:37:00 +0000

    extra fixes from the git plugin i used + roh's validation fix

--- app/admin.py
+++ app/admin.py
@@ -311,11 +311,9 @@ def endpoint4():
                 round_matches = tournament_matches[gender][round_num]
                 round_players = set() #used for finding duplicate
 
-
                 if len(round_matches) != expected_matches:
                     return [f"Invalid match count in {gender} match {match} in round {round_num} in {tournament_name}"]
                 expected_matches = expected_matches // 2
-
 
                 for match in round_matches:
                     try:
