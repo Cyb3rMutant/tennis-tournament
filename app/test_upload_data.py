17e1aefae28814b5c2450640469c5fa8e79693aa
Rohaan Aslam <rohaan2.aslam@live.uwe.ac.uk>
2023-04-17 16:15:00 +0100

    added unit test support for linxu

--- app/test_upload_data.py
+++ app/test_upload_data.py
@@ -89,7 +89,7 @@ def test_endpoint3(file_names, expected):
 
 
 import os
-test_files_0 = os.listdir('app/data_app/endpoint4/test0_correct')
+test_files_0 = sorted(os.listdir('app/data_app/endpoint4/test0_correct'))
 endpoint_4_test_data_test0 = [
     (test_files_0, ''),
 ]
@@ -113,7 +113,7 @@ def test_endpoint4_0(file_names, expected):
 
 
 
-test_files_1 = os.listdir('app/data_app/endpoint4/test1_normal')
+test_files_1 = sorted(os.listdir('app/data_app/endpoint4/test1_normal'))
 endpoint_4_test_data_test1 = [
     (test_files_1, 
     ["Invalid score in ladies match ['FP18' 2 'FP26' 2] in round 2 in TAC1", 
@@ -138,7 +138,7 @@ def test_endpoint4_1(file_names, expected):
 
 
 
-test_files_2 = os.listdir('app/data_app/endpoint4/test2')
+test_files_2 = sorted(os.listdir('app/data_app/endpoint4/test2'))
 endpoint_4_test_data_test2 = [
     (test_files_2, ['Number of files uploaded should be 40 not 41']),
 ]
@@ -161,7 +161,7 @@ def test_endpoint4_2(file_names, expected):
 
 
 
-test_files_3 = os.listdir('app/data_app/endpoint4/test3')
+test_files_3 = sorted(os.listdir('app/data_app/endpoint4/test3'))
 endpoint_4_test_data_test3 = [
     (test_files_3,["""Invalid file name TAC1 ROUND 2 MEN I AM IMPOSTER.csv: ['File name should be formatted in `TNAME "ROUND" RNUM GENDER`.']"""]),
 ]
@@ -183,7 +183,7 @@ def test_endpoint4_3(file_names, expected):
 
 
 
-test_files_4 = os.listdir('app/data_app/endpoint4/test4')
+test_files_4 = sorted(os.listdir('app/data_app/endpoint4/test4'))
 endpoint_4_test_data_test4 = [
     (test_files_4,
     ["Invalid file name SS ROUND 3 MEN.csv: ['Invalid tournament name SS.']",
@@ -210,7 +210,7 @@ def test_endpoint4_4(file_names, expected):
 
 
 
-test_files_5 = os.listdir('app/data_app/endpoint4/test5')
+test_files_5 = sorted(os.listdir('app/data_app/endpoint4/test5'))
 endpoint_4_test_data_test5 = [
     (test_files_5,
     ["""Invalid player name in men's match ['SP23' 3 'MP22' 0] in round 3 in TAC1""",
@@ -233,7 +233,7 @@ def test_endpoint4_5(file_names, expected):
 
 
 
-test_files_6 = os.listdir('app/data_app/endpoint4/test6')
+test_files_6 = sorted(os.listdir('app/data_app/endpoint4/test6'))
 endpoint_4_test_data_test6 = [
     (test_files_6,['Invalid number of matches  in men  round 5 in TAW11'] ),
 ]
